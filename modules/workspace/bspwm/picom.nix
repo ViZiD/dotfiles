7afd854e420ee3072e853761d95669a8710c07e2
Radik Islamov <vizid1337@gmail.com>
2023-12-14 05:02:29 +0500

    fix picom flashing after switch windows

--- modules/workspace/bspwm/picom.nix
+++ modules/workspace/bspwm/picom.nix
@@ -2,6 +2,9 @@
   home-manager.users.radik = {
     services.picom = {
       enable = true;
+      fade = true;
+      fadeSteps = [ 1 1 ];
+      fadeDelta = 50;
       settings = {
         # fix this ugly shit
         # https://github.com/orgs/regolith-linux/discussions/949
