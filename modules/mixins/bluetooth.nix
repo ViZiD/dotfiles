a14f78f0faeb811447a904d3352fbfe672f5fa04
Radik Islamov <vizid1337@gmail.com>
2024-01-29 22:45:09 +0500

    move bluetooth module to mixins

---
+++ modules/mixins/bluetooth.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }:
+{
+  hardware.bluetooth = {
+    enable = true;
+    package = pkgs.bluez;
+
+    settings = {
+      General = {
+        Enable = "Source,Sink,Media,Socket";
+        Experimental = true;
+      };
+    };
+  };
+}
