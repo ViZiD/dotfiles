e50ea78bb5e8e4c7c547cd6e9e30975460437b34
Radik Islamov <vizid1337@gmail.com>
2024-03-09 04:35:12 +0500

    fix steam

--- modules/apps/gaming/default.nix
+++ modules/apps/gaming/default.nix
@@ -1,9 +1,13 @@
 { pkgs, config, ... }: {
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
   home-manager.users.radik = {
     home.packages = with pkgs; [
       config.nur.repos.materus.polymc
       steamguard-cli
-      steam
     ];
   };
 }
