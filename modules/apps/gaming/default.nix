414ed563ad242d4aa44e06ca2e190eaf2fc2ce68
Radik Islamov <vizid1337@gmail.com>
2024-03-09 05:35:34 +0500

    add steam proton-ge

--- modules/apps/gaming/default.nix
+++ modules/apps/gaming/default.nix
@@ -3,7 +3,11 @@
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    extraCompatPackages = with pkgs.nix-gaming; [
+      proton-ge
+    ];
   };
+
   home-manager.users.radik = {
     home.packages = with pkgs; [
       config.nur.repos.materus.polymc
