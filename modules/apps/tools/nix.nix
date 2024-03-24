02ab96734ae5f400eea34b40bd62da56e0f2dcb5
Radik Islamov <vizid1337@gmail.com>
2024-03-24 20:32:51 +0500

    add nix-init config

--- modules/apps/tools/nix.nix
+++ modules/apps/tools/nix.nix
@@ -1,20 +1,29 @@
 { pkgs, ... }:
-
+let
+  toTOML = name: (pkgs.formats.toml { }).generate "${name}";
+in
 {
-  environment.systemPackages = with pkgs; [
-    cachix
+  home-manager.users.radik = {
+    xdg.configFile."nix-init/config.toml".source = toTOML "config.toml" {
+      commit = true;
+      maintainers = [ "vizid" ];
+    };
+    home.packages = with pkgs; [
+      cachix
 
-    # nix lsp
-    nil
-    nixd
+      # nix lsp
+      nil
+      nixd
 
-    nixpkgs-fmt
+      nixpkgs-fmt
 
-    nixpkgs-review
+      nixpkgs-review
 
-    # generators
-    nix-init
-    nurl
-    nodePackages.node2nix
-  ];
+      # generators
+      nix-init
+      nurl
+      nodePackages.node2nix
+    ];
+  };
 }
+
