7e71eb501ea98a1c25a579dcb07b3bad15a5cc14
Radik Islamov <vizid1337@gmail.com>
2024-05-05 07:07:43 +0500

    remove deprecated activation options

--- modules/workspace/base.nix
+++ modules/workspace/base.nix
@@ -6,13 +6,15 @@
       userDirs.enable = true;
     };
 
+
     home = {
 
-      activation."mimeapps-remove" = {
-        before = [ "linkGeneration" ];
-        after = [ ];
-        data = "rm -f /home/radik/.config/mimeapps.list";
-      };
+ # deprecated...
+    #   activation."mimeapps-remove" = {
+    #     before = [ "linkGeneration" ];
+    #     after = [ ];
+    #     data = "rm -f /home/radik/.config/mimeapps.list";
+    #   };
 
       packages = with pkgs; [ flameshot ];
     };
