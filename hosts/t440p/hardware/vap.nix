53250d1ab860381677bff964697e593fea6df25d
Radik Islamov <vizid1337@gmail.com>
2024-07-31 04:08:09 +0500

    switch to iHD driver

--- hosts/t440p/hardware/vap.nix
+++ hosts/t440p/hardware/vap.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }:
 {
   environment.sessionVariables = {
-    LIBVA_DRIVER_NAME = "i965";
+    LIBVA_DRIVER_NAME = "iHD";
   };
   hardware.graphics = with pkgs; {
-    extraPackages = [ intel-vaapi-driver ];
-    extraPackages32 = [ pkgsi686Linux.intel-vaapi-driver ];
+    extraPackages = [ intel-media-driver ];
+    extraPackages32 = [ driversi686Linux.intel-media-driver ];
   };
 }
