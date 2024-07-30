0b0b2a7bd5ce2ec5bb555cf7f83d95eea85f0b68
Radik Islamov <vizid1337@gmail.com>
2024-07-31 04:10:23 +0500

    add sane support

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   hardware = {
     # Enable all the firmware
@@ -6,6 +7,14 @@
     # Enable all the firmware with a license allowing redistribution
     # (i.e. free firmware and firmware-linux-nonfree)
     enableRedistributableFirmware = true;
+    sane = {
+      enable = true;
+      extraBackends = with pkgs; [
+        sane-airscan
+        hplipWithPlugin
+      ];
+      netConf = "172.16.1.2";
+    };
   };
 
   services.fwupd.enable = true;
