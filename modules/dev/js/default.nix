f2c93701537af8dc067f52ade1ce0e14fa4fe1e8
Radik Islamov <vizid1337@gmail.com>
2024-08-04 10:37:09 +0500

    add rush npm package update webcrack

--- modules/dev/js/default.nix
+++ modules/dev/js/default.nix
@@ -2,7 +2,11 @@
 
 let
   genPkgs = pkgs.callPackage ./packages { inherit pkgs; };
-  extPkgs = with genPkgs; [ webcrack ];
+
+  extPkgs = with genPkgs // { rush-microsoft = genPkgs."@microsoft/rush"; }; [
+    webcrack
+    rush-microsoft
+  ];
   nixPkgs = with pkgs.nodePackages; [
     # http-server
     # rollup
