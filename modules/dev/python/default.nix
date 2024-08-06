fae3f39a30b80f87ee40c7a522c1af9c3552ed39
Radik Islamov <vizid1337@gmail.com>
2024-08-06 10:57:13 +0500

    switch to python 3.11

--- modules/dev/python/default.nix
+++ modules/dev/python/default.nix
@@ -2,8 +2,9 @@
 {
   home-manager.users.radik = {
     home.packages = with pkgs; [
-      (python312.withPackages (ps: [ virtualenv ]))
+      (python311.withPackages (ps: [ virtualenv ]))
       pdm
+      poetry
       # mypy
     ];
   };
