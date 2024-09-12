44e565cff1aebc3f08acbf4db9d056c06ed17905
Radik Islamov <vizid1337@gmail.com>
2024-09-12 07:09:17 +0500

    remove auto gc store

--- modules/nix.nix
+++ modules/nix.nix
@@ -45,12 +45,6 @@
       ];
     };
 
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 30d";
-    };
-
     optimise = {
       # Automatically run the nix optimiser at a specific time
       automatic = true;
