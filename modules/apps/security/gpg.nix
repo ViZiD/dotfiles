079066fb825bc1884da6a008447a9955039536e0
Radik Islamov <vizid1337@gmail.com>
2024-09-14 18:31:51 +0500

    gpg-agent: remove unused config

--- modules/apps/security/gpg.nix
+++ modules/apps/security/gpg.nix
@@ -6,11 +6,6 @@
 }:
 let
   enableGpg = true;
-  gpgPkg = config.home-manager.users.radik.programs.gpg.package;
-  gpgInitStr = ''
-    export GPG_TTY="$(tty)"
-    ${gpgPkg}/bin/gpg-connect-agent updatestartuptty /bye > /dev/null
-  '';
 in
 with lib;
 mkIf enableGpg {
@@ -25,25 +20,9 @@ mkIf enableGpg {
       maxCacheTtlSsh = maxCacheTtl;
       sshKeys = [ "4B4E301406294B81FED02CA5DB6837BB5463F58E" ];
     };
-    systemd.user.services.gpg-agent = {
-      Service = {
-        Environment = lib.mkForce [
-          "GPG_TTY=/dev/tty1"
-          "DISPLAY=:0"
-          "GNUPGHOME=${config.home-manager.users.radik.xdg.dataHome}/gnupg"
-        ];
-      };
-    };
     programs.gpg = {
       enable = true;
       homedir = "${config.home-manager.users.radik.xdg.dataHome}/gnupg";
     };
-
-    programs.zsh.initExtra = gpgInitStr;
-    home.sessionVariablesExtra = ''
-      if [[ -z "$SSH_AUTH_SOCK" ]]; then
-        export SSH_AUTH_SOCK="$(${gpgPkg}/bin/gpgconf --list-dirs agent-ssh-socket)"
-      fi
-    '';
   };
 }
