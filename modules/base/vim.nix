31781016eb687d379b54d3ea8b1b6ead5a791ba3
Radik Islamov <mail@vizqq.cc>
2024-11-17 18:22:45 +0500

    vim: add himalaya cli plugin

--- modules/base/vim.nix
+++ modules/base/vim.nix
@@ -8,6 +8,7 @@ with lib;
 let
   cfg = config.dots.base.vim;
   user = config.dots.user;
+  hmConfig = config.home-manager.users.${user.username};
   isStylesEnabled = config.dots.styles.enable;
 in
 {
@@ -21,10 +22,25 @@ in
       stylix.targets = mkIf isStylesEnabled {
         neovim.enable = true;
       };
-      programs.neovim = {
-        enable = true;
-
-      };
+      programs.neovim =
+        {
+          enable = true;
+          extraConfig = ''
+            syntax on
+            filetype plugin on
+          '';
+        }
+        // optionalAttrs (user.mail.enable) {
+          plugins = with pkgs.vimPlugins; [
+            {
+              plugin = himalaya-vim;
+              config = ''
+                let g:himalaya_executable = "${getExe hmConfig.programs.himalaya.package}"
+                let g:himalaya_config_path = "$HOME/.config/himalaya/config.toml"
+              '';
+            }
+          ];
+        };
     };
   };
 }
