66507dba0a6ee2b3aeb5d2fe62d451765123288b
Radik Islamov <mail@vizqq.cc>
2024-12-06 21:26:02 +0500

    feat(bluetooth): enable persist

--- modules/shared/bluetooth.nix
+++ modules/shared/bluetooth.nix
@@ -8,11 +8,18 @@ with lib;
 let
   cfg = config.dots.shared.bluetooth;
   user = config.dots.user;
+  isPersistEnabled = config.dots.shared.persist.enable;
   headphones = "18:AA:0F:C9:D0:29";
 in
 {
   options.dots.shared.bluetooth.enable = mkEnableOption "Enable bluetooth support";
   config = mkIf cfg.enable {
+    dots.shared.persist.system = mkIf isPersistEnabled {
+      directories = [
+        "/var/lib/bluetooth"
+      ];
+    };
+
     hardware.bluetooth = {
       enable = true;
       package = pkgs.bluez;
