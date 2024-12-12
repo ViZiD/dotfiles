acfeacd248061ffc9370d083df1cc7f786a97875
Radik Islamov <mail@vizqq.cc>
2024-12-12 20:24:02 +0500

    fix(zapret): fix youtube

--- modules/shared/zapret.nix
+++ modules/shared/zapret.nix
@@ -1,4 +1,5 @@
 {
+  self,
   config,
   lib,
   ...
@@ -17,17 +18,18 @@ in
       udpPorts = [
         "50000:50099"
         "443"
-        "80"
       ];
-      # blacklist = [
-      #   "ru-board.com"
-      # ];
       params = [
         "--filter-udp=50000-50099"
         "--dpi-desync=fake"
         "--dpi-desync-any-protocol"
         "--new"
-        "--filter-tcp=443,80"
+        "--filter-udp=443"
+        "--dpi-desync-fake-quic=${self}/modules/shared/quic_initial_www_google_com.bin"
+        "--dpi-desync=fake"
+        "--dpi-desync-repeats=2"
+        "--new"
+        "--filter-tcp=80,443"
         "--dpi-desync=fake,multidisorder"
         "--dpi-desync-ttl=3"
       ];
