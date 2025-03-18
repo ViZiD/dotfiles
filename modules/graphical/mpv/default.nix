67a11028f317151ed4b3f375cf41ae3203c33e58
Radik Islamov <mail@vizqq.cc>
2025-03-18 20:11:25 +0500

    refactor(mpv): change mime config structure

--- modules/graphical/mpv/default.nix
+++ modules/graphical/mpv/default.nix
@@ -9,6 +9,27 @@ let
   cfg = config.dots.graphical.mpv;
   user = config.dots.user;
   isPersistEnabled = config.dots.shared.persist.enable;
+
+  associations = {
+    "video/mp4" = "mpv.desktop";
+    "video/x-matroska" = "mpv.desktop";
+    "video/webm" = "mpv.desktop";
+    "video/quicktime" = "mpv.desktop";
+    "video/x-msvideo" = "mpv.desktop";
+    "video/x-ms-wmv" = "mpv.desktop";
+    "video/x-flv" = "mpv.desktop";
+    "video/x-m4v" = "mpv.desktop";
+    "video/3gpp" = "mpv.desktop";
+    "video/3gpp2" = "mpv.desktop";
+    "video/x-matroska-3d" = "mpv.desktop";
+    "video/x-ms-asf" = "mpv.desktop";
+    "video/x-ms-wvx" = "mpv.desktop";
+    "video/x-ms-wmx" = "mpv.desktop";
+    "video/x-ms-wm" = "mpv.desktop";
+    "video/x-ms-wmp" = "mpv.desktop";
+    "video/x-ms-wmz" = "mpv.desktop";
+
+  };
 in
 {
   options.dots.graphical.mpv.enable = mkEnableOption "Enable mpv player";
@@ -135,24 +156,9 @@ in
           };
         };
       };
-      xdg.mimeApps.defaultApplications = {
-        "video/mp4" = "mpv.desktop";
-        "video/x-matroska" = "mpv.desktop";
-        "video/webm" = "mpv.desktop";
-        "video/quicktime" = "mpv.desktop";
-        "video/x-msvideo" = "mpv.desktop";
-        "video/x-ms-wmv" = "mpv.desktop";
-        "video/x-flv" = "mpv.desktop";
-        "video/x-m4v" = "mpv.desktop";
-        "video/3gpp" = "mpv.desktop";
-        "video/3gpp2" = "mpv.desktop";
-        "video/x-matroska-3d" = "mpv.desktop";
-        "video/x-ms-asf" = "mpv.desktop";
-        "video/x-ms-wvx" = "mpv.desktop";
-        "video/x-ms-wmx" = "mpv.desktop";
-        "video/x-ms-wm" = "mpv.desktop";
-        "video/x-ms-wmp" = "mpv.desktop";
-        "video/x-ms-wmz" = "mpv.desktop";
+      xdg.mimeApps = {
+        defaultApplications = associations;
+        associations.added = associations;
       };
     };
   };
