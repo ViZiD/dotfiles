98535ae6c6d4b4c3083c37bf7178a3d98a2f7c0e
Radik Islamov <mail@vizqq.cc>
2025-03-19 04:32:55 +0500

    feat(tools): add swayimg

--- modules/graphical/tools/default.nix
+++ modules/graphical/tools/default.nix
@@ -16,6 +16,30 @@ let
     "application/rar" = "org.gnome.FileRoller";
     "application/7z" = "org.gnome.FileRoller";
     "application/*tar" = "org.gnome.FileRoller";
+
+    "image/bmp" = "swayimg.desktop";
+    "image/gif" = "swayimg.desktop";
+    "image/jpeg" = "swayimg.desktop";
+    "image/jpg" = "swayimg.desktop";
+    "image/pjpeg" = "swayimg.desktop";
+    "image/png" = "swayimg.desktop";
+    "image/svg+xml" = "swayimg.desktop";
+    "image/tiff" = "swayimg.desktop";
+    "image/webp" = "swayimg.desktop";
+    "image/x-bmp" = "swayimg.desktop";
+    "image/x-emf" = "swayimg.desktop";
+    "image/x-freehand" = "swayimg.desktop";
+    "image/x-ico" = "swayimg.desktop";
+    "image/x-pcx" = "swayimg.desktop";
+    "image/x-png" = "swayimg.desktop";
+    "image/x-portable-anymap" = "swayimg.desktop";
+    "image/x-portable-bitmap" = "swayimg.desktop";
+    "image/x-portable-graymap" = "swayimg.desktop";
+    "image/x-portable-pixmap" = "swayimg.desktop";
+    "image/x-tga" = "swayimg.desktop";
+    "image/x-wmf" = "swayimg.desktop";
+    "image/x-xbitmap" = "swayimg.desktop";
+    "image/xpm" = "swayimg.desktop";
   };
 in
 {
@@ -28,11 +52,17 @@ in
     };
     home-manager.users.${user.username} = mkIf user.enable {
       # stylix.targets = mkIf isStylesEnabled { };
-      home.packages = with pkgs; [ file-roller ];
+      home.packages = with pkgs; [
+        file-roller
+        swayimg
+      ];
 
-      xdg.mimeApps = {
-        defaultApplications = associations;
-        associations.added = associations;
+      xdg = {
+        configFile."swayimg/config".source = ./swayimgrc;
+        mimeApps = {
+          defaultApplications = associations;
+          associations.added = associations;
+        };
       };
     };
   };
