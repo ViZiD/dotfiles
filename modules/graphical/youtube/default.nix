b6757fa56c8643e81674d0a123c1eabd74422da5
Radik Islamov <mail@vizqq.cc>
2025-03-20 04:48:26 +0500

    chore!(youtube): remove freetube

--- modules/graphical/youtube/default.nix
+++ modules/graphical/youtube/default.nix
@@ -8,42 +8,22 @@ with lib;
 let
   cfg = config.dots.graphical.youtube;
   user = config.dots.user;
-  isPersistEnabled = config.dots.shared.persist.enable;
 in
+# isPersistEnabled = config.dots.shared.persist.enable;
 {
   options.dots.graphical.youtube.enable = mkEnableOption "Enable youtube stuff";
 
   config = mkIf cfg.enable {
-    dots.shared.persist.user = mkIf isPersistEnabled {
-      directories = [
-        ".config/FreeTube"
-      ];
-    };
+    # dots.shared.persist.user = mkIf isPersistEnabled {
+    #   directories = [
+    #     ".config/FreeTube"
+    #   ];
+    # };
     home-manager.users.${user.username} = mkIf user.enable {
       home.packages = with pkgs; [
         yt-dlp
         ytfzf
       ];
-      programs.freetube = {
-        enable = true;
-        settings = {
-          checkForUpdates = false;
-          checkForBlogPosts = false;
-
-          hideTrendingVideos = true;
-          hidePopularVideos = true;
-          hideVideoLikesAndDislikes = true;
-          hideHeaderLogo = true;
-
-          useSponsorBlock = true;
-          useDeArrowTitles = true;
-          useDeArrowThumbnails = true;
-
-          externalPlayer = "mpv";
-          defaultQuality = "1080";
-          disableSmoothScrolling = true;
-        };
-      };
     };
   };
 }
