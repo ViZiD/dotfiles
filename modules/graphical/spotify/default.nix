feea7d3086d8b4b9454758514ae760003249be76
Radik Islamov <mail@vizqq.cc>
2025-05-04 23:43:49 +0500

    chore(spotify): add config for spotify-player

--- modules/graphical/spotify/default.nix
+++ modules/graphical/spotify/default.nix
@@ -30,6 +30,18 @@ in
       programs = {
         spotify-player = {
           enable = true;
+          settings = {
+            device = {
+              volume = 100;
+              autoplay = true;
+            };
+          };
+          keymaps = [
+            {
+              command = "Repeat";
+              key_sequence = "g .";
+            }
+          ];
         };
         spicetify = {
           enable = true;
