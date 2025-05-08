da055076f1e788acada34872f6f9f0f98e838760
Radik Islamov <mail@vizqq.cc>
2025-05-09 02:55:11 +0500

    chore(niri): add xdg portal config

--- modules/graphical/niri/default.nix
+++ modules/graphical/niri/default.nix
@@ -34,8 +34,20 @@ in
     };
 
     xdg.portal = {
-      config.common = {
-        "org.freedesktop.impl.portal.FileChooser" = "gtk";
+      config = {
+        niri = {
+          default = [
+            "gnome"
+            "gtk"
+          ];
+          "org.freedesktop.impl.portal.Access" = [ "gtk" ];
+          "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+          "org.freedesktop.impl.portal.Notification" = [ "gtk" ];
+          "org.freedesktop.impl.portal.RemoteDesktop" = [ "gnome" ];
+          "org.freedesktop.impl.portal.ScreenCast" = [ "gnome" ];
+          "org.freedesktop.impl.portal.Screenshot" = [ "gnome" ];
+          "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
+        };
       };
       extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
     };
