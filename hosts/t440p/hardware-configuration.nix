5acff316f62d31a6d1691cb28d3303179519b23a
Radik Islamov <mail@vizqq.cc>
2025-05-10 01:18:56 +0500

    misc(t440p): remove comments

--- hosts/t440p/hardware-configuration.nix
+++ hosts/t440p/hardware-configuration.nix
@@ -32,26 +32,6 @@
     "snd_hda_intel.power_save=1"
   ];
 
-  # fileSystems."/" = {
-  #   device = "/dev/disk/by-uuid/cbe3c413-c8bd-4168-90d3-f7ea592b3c4e";
-  #   fsType = "ext4";
-  # };
-
-  # boot.initrd.luks.devices."luks-fcd9a6bb-9d5b-4ec4-b88c-672b9f43fd35".device = "/dev/disk/by-uuid/fcd9a6bb-9d5b-4ec4-b88c-672b9f43fd35";
-
-  # fileSystems."/boot" = {
-  #   device = "/dev/disk/by-uuid/748E-9AAA";
-  #   fsType = "vfat";
-  #   options = [
-  #     "fmask=0022"
-  #     "dmask=0022"
-  #   ];
-  # };
-
-  # swapDevices = [
-  #   { device = "/dev/disk/by-uuid/9ac545a1-8519-4a9b-8422-0be19f15f53e"; }
-  # ];
-
   boot.initrd.postDeviceCommands = pkgs.lib.mkBefore ''
     mkdir -p /mnt
 
