52f40230ad6f4ec15fb93e7dc70bb38abcac2523
Radik Islamov <mail@vizqq.cc>
2025-05-18 06:36:39 +0500

    feat(git): revork git config

--- README.md
+++ README.md
@@ -3,8 +3,10 @@
 ![Я настроил!!!](/assets/bgg.jpg)
 
 ### Thanks
-* [balsoft/nixos-config](https://github.com/balsoft/nixos-config)
-* [vyorkin/nixos-config](https://github.com/vyorkin/nixos-config)
-* [etu/nixconfig](https://github.com/etu/nixconfig)
-* [TheMaxMur/NixOS-Configuration](https://github.com/TheMaxMur/NixOS-Configuration)
-* [Misterio77/nix-config](https://github.com/Misterio77/nix-config)
+
+- [balsoft/nixos-config](https://github.com/balsoft/nixos-config)
+- [vyorkin/nixos-config](https://github.com/vyorkin/nixos-config)
+- [etu/nixconfig](https://github.com/etu/nixconfig)
+- [TheMaxMur/NixOS-Configuration](https://github.com/TheMaxMur/NixOS-Configuration)
+- [Misterio77/nix-config](https://github.com/Misterio77/nix-config)
+- [name-snrl/nixos-configuration](https://github.com/name-snrl/nixos-configuration)
--- modules/cli/git.nix
+++ modules/cli/git.nix
@@ -30,23 +30,53 @@ in
         ff = "merge --ff-only";
         graph = "log --decorate --oneline --graph";
         pushall = "!git remote | xargs -L1 git push --all";
+        cb = "switch -c";
+        au = "remote add -f upstream";
       };
+      # some hacks for large repos
+      # https://www.git-scm.com/docs/partial-clone
+      aliases.au-promisor = "!git remote add upstream \"$@\" && shift \"$#\" && git fetch --filter=blob:none upstream";
+      aliases.clone-promisor = "clone --filter=blob:none --no-checkout";
+      extraConfig.clone.filterSubmodules = true;
+
       extraConfig = {
         init.defaultBranch = "master";
-        pull.rebase = true;
-        rebase.autoStash = true;
+
+        url = {
+          "git@github.com:".pushInsteadOf = "https://github.com/";
+          "git@gist.github.com:".pushInsteadOf = "https://gist.github.com/";
+        };
+
         http.postBuffer = 524288000;
         credential.helper = "store";
         commit.gpgsign = true;
 
         merge.conflictStyle = "zdiff3";
+        merge.autoStash = true;
+        rebase.autoStash = true;
         commit.verbose = true;
+
         diff.algorithm = "histogram";
         log.date = "iso";
         column.ui = "auto";
         branch.sort = "committerdate";
-        push.autoSetupRemote = true;
-        rerere.enabled = true;
+        # triangular workflow
+        #
+        # 1. clone your fork
+        # 2. configure upstream using git au/au-promisor aliases, see above
+        # 3. on branch createion branch off from upstream:
+        #    `git cb <new_name> upstream/master`
+        #
+        # now you will push to origin and pull from upstream by default. this
+        # makes it easier to stay in sync with upstream, while still pushing your
+        # changes to origin
+        push = {
+          followtags = true;
+          autoSetupRemote = true;
+          default = "current";
+        };
+
+        remote.pushdefault = "origin";
       };
       signing = {
         signByDefault = true;
