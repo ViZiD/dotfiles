e94cd24e8a898cb0f3e53d76c8daa3f86fd510b2
Radik Islamov <mail@vizqq.cc>
2025-05-23 17:22:46 +0500

    feat(gpg): add more gpg settings

--- modules/cli/gpg.nix
+++ modules/cli/gpg.nix
@@ -66,6 +66,26 @@ in
             }
           )
         ];
+        settings = {
+          fixed-list-mode = true;
+          no-comments = true;
+          no-emit-version = true;
+          no-greeting = true;
+          with-fingerprint = true;
+          require-cross-certification = true;
+          no-symkey-cache = true;
+          charset = "utf-8";
+          keyid-format = "0xlong";
+          list-options = "show-uid-validity";
+          verify-options = "show-uid-validity";
+          personal-cipher-preferences = "AES256 AES192 AES";
+          personal-digest-preferences = "SHA512 SHA384 SHA256";
+          personal-compress-preferences = "ZLIB BZIP2 ZIP Uncompressed";
+          default-preference-list = "SHA512 SHA384 SHA256 AES256 AES192 AES ZLIB BZIP2 ZIP Uncompressed";
+          cert-digest-algo = "SHA512";
+          s2k-digest-algo = "SHA512";
+          s2k-cipher-algo = "AES256";
+        };
       };
     };
 
