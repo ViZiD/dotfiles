cbd228ef0833e7b5bc74cb0b98eae19c9184d03d
Radik Islamov <mail@vizqq.cc>
2025-07-04 19:41:31 +0500

    feat(games): add steam

--- modules/graphical/games/default.nix
+++ modules/graphical/games/default.nix
@@ -17,10 +17,21 @@ in
     dots.shared.persist.user = mkIf isPersistEnabled {
       directories = [
         ".local/share/PrismLauncher"
+        ".steam"
+        ".local/share/Steam"
       ];
     };
+    programs.steam = {
+      enable = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    };
     home-manager.users.${user.username} = mkIf user.enable {
-      home.packages = [ pkgs.prismlauncher ];
+      home.packages = with pkgs; [
+        prismlauncher
+        steamguard-cli
+      ];
     };
   };
 }
