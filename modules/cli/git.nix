1bf53e6d1619611fd3c5b5f469ce5abd7d3a16d3
Radik Islamov <mail@vizqq.cc>
2026-03-23 02:05:48 +0500

    fix(git): cleanup config

--- modules/cli/git.nix
+++ modules/cli/git.nix
@@ -13,10 +13,6 @@ in
   options.dots.cli.git.enable = mkEnableOption "Enable git";
   config = mkIf cfg.enable {
 
-    environment.systemPackages = [
-      pkgs.git
-    ];
-
     home-manager.users.${user.username} = mkIf user.enable {
       home.packages = [ pkgs.tig ];
       programs = {
@@ -55,25 +51,18 @@ in
               pushall = "!git remote | xargs -L1 git push --all";
               cb = "switch -c";
               au = "remote add -f upstream";
-
-              # some hacks for large repos
-              # https://www.git-scm.com/docs/partial-clone
-              au-promisor = "!git remote add upstream \"$@\" && shift \"$#\" && git fetch --filter=blob:none upstream";
-              clone-promisor = "clone --filter=blob:none --no-checkout";
             };
-            clone.filterSubmodules = true;
 
             init.defaultBranch = "master";
 
             url = {
               "git@github.com:".pushInsteadOf = "https://github.com/";
               "git@gist.github.com:".pushInsteadOf = "https://gist.github.com/";
-              "git.debian.org:/git/".insteadOf = "https://anonscm.debian.org/git/";
               "git@salsa.debian.org:".insteadOf = "https://salsa.debian.org/";
             };
 
             http.postBuffer = 524288000;
-            credential.helper = "store";
+            credential.helper = "cache --timeout=86400";
             commit.gpgsign = true;
 
             merge.conflictStyle = "diff3";
@@ -83,13 +72,12 @@ in
 
             diff.algorithm = "histogram";
             log.date = "iso";
-            column.ui = "auto";
-            branch.sort = "committerdate";
+            branch.sort = "-committerdate";
             # triangular workflow
             #
             # 1. clone your fork
-            # 2. configure upstream using git au/au-promisor aliases, see above
-            # 3. on branch createion branch off from upstream:
+            # 2. configure upstream using `git au <url>`
+            # 3. on branch creation branch off from upstream:
             #    `git cb <new_name> upstream/master`
             #
             # now you will push to origin and pull from upstream by default. this
